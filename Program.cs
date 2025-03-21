eaaa70af353bced223aba2af2707e4b5700a6be1
unknown <yalshaeyr@hotmail.com>
2025-03-21 20:53:09 +1100

    Register db context and endpoints

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ProgressVisualiserApi.Database.Contexts;
+using ProgressVisualiserApi.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var connection = string.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+
+builder.Services.AddDbContext<MetricsContext>(options =>
+    options.UseSqlServer(connection));
+
+builder.Services.AddDbContext<MetricDataContext>(options =>
+    options.UseSqlServer(connection));
+
+app.RegisterMetricEndpoints();
+app.RegisterMetricDataEndpoints();
 
 app.Run();
