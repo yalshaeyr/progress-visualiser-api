1ded45ab63135c75e8db782ad127515278db29a5
unknown <yalshaeyr@hotmail.com>
2025-03-31 23:42:46 +1100

    Enable CORS

--- ProgressVisualiserApi/Program.cs
+++ ProgressVisualiserApi/Program.cs
@@ -3,16 +3,36 @@ using Azure.Monitor.OpenTelemetry.AspNetCore;
 using ProgressVisualiserApi.Database;
 using ProgressVisualiserApi.Controllers;
 
+var AllowSpecificOrigins = "_allowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins(
+                [
+                    "http://localhost:5173",
+                    "https://red-tree-0360f6300.6.azurestaticapps.net"
+                ]
+            )
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var sqlConnectionString = string.Empty;
 var appInsightsConnectionString = string.Empty;
+
 if (builder.Environment.IsDevelopment())
 {
     builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
     sqlConnectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
     appInsightsConnectionString = builder.Configuration.GetConnectionString("AZURE_APPLICATIONINSIGHTS_CONNECTIONSTRING");
 }
+
 else
 {
     sqlConnectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
@@ -30,6 +50,8 @@ var app = builder.Build();
 
 app.RegisterMetricController();
 app.RegisterMetricDataController();
+
+app.UseCors(AllowSpecificOrigins);
 
 app.Run();
 
